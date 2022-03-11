cc562a9fccc38c830df598553ddf01ed920fc355
erickw00 <erik@renderheads.com>
2022-03-11 11:07:20 +0300

    @rua/frontend[bug] fix type errors in inbox.tsx around paddle class

--- pages/inbox.tsx
+++ pages/inbox.tsx
@@ -22,6 +22,30 @@ type Props = {
   };
 };
 
+declare let process: {
+  env: {
+    NEXT_PUBLIC_PADDLE_PRODUCT_ID: string;
+    NEXT_PUBLIC_PADDLE_VENDOR_ID: string;
+    NEXT_PUBLIC_PADDLE_ENV: string;
+  };
+};
+
+declare let window: {
+  Paddle: {
+    Setup: (options: { vendor: number; debug?: boolean }) => void;
+    Checkout: {
+      open: (options: {
+        product: number;
+        email: string;
+        successCallback: (data: any, err: Error) => void;
+      }) => void;
+    };
+    Environment: {
+      set: (env: string) => void;
+    };
+  };
+};
+
 const Inbox: NextPage<Props> = ({ profile }) => {
   const { updateProfile } = useAuth();
 
