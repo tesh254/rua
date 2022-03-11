884560ef35fc419736821e11608017f84a754299
erickw00 <erik@renderheads.com>
2022-03-11 11:04:38 +0300

    @rua/frontend[bug] move types to next-env.d.ts file to prevent typescript build errors

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,28 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare let process: {
+    env: {
+      NEXT_PUBLIC_PADDLE_PRODUCT_ID: string;
+      NEXT_PUBLIC_PADDLE_VENDOR_ID: string;
+      NEXT_PUBLIC_PADDLE_ENV: string;
+    }
+}
+  
+declare let window: {
+  Paddle: {
+    Setup: (options: { vendor: string; debug: boolean }) => void;
+    Checkout: {
+      open: (options: {
+        product: string;
+        email: string;
+        successCallback: (data: any, err: Error) => void;
+      }) => void;
+    };
+    Environment: {
+      set: (env: string) => void;
+    }
+  };
+};
+  
